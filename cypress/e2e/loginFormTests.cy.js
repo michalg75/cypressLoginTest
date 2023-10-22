e8f8e470ce12b71ba6322f5c27b366b2cc0849c8
michal gajewski <michu214@gmail.com>
2023-10-22 16:54:56 +0200

    small refactor

--- cypress/e2e/loginFormTests.cy.js
+++ cypress/e2e/loginFormTests.cy.js
@@ -1,33 +1,33 @@
 import loginPage from "../support/pageObjects/adminStorePage/loginPage"
-import { incorrectMail, incorrectPassword, correctCredentials, alertBoxMessages }
-    from "../fixtures/adminPage/loginPage.json"
 import loginPageSelectors from "../support/selectors/loginPageSelectors"
+import loginData from "../fixtures/loginPage.json"
 
-describe('login form tests', ()=>{
+describe('login form tests', () => {
 
-    beforeEach(() => {
+    beforeEach(function () {
         loginPage.visitLoginPage()
     })
+
     afterEach(() => {
         cy.clearLocalStorage()
     })
 
-    it('asserts login success', ()=>{
-        loginPage.insertLoginDataAndClickSubmit(correctCredentials.email, correctCredentials.password)
+    it('asserts login success', () => {
+        loginPage.insertLoginDataAndClickSubmit(loginData.correctCredentials.email, loginData.correctCredentials.password)
         cy.url().should('eq', Cypress.config('baseUrl') + Cypress.config('panelMainPage'))
     })
 
-    it('asserts failed login with wrong mail', ()=>{
-        loginPage.insertLoginDataAndClickSubmit(incorrectMail.mail, correctCredentials.password)
+    it('asserts failed login with wrong mail', () => {
+        loginPage.insertLoginDataAndClickSubmit(loginData.incorrectMail.mail, loginData.correctCredentials.password)
         loginPageSelectors.alertBox().invoke('text').then(text => {
-            expect(text.trim()).to.eq(alertBoxMessages.baseAlert + alertBoxMessages.wrongMailAlert)
+            expect(text.trim()).to.eq(loginData.alertBoxMessages.baseAlert + loginData.alertBoxMessages.wrongMailAlert)
         })
     })
 
-    it('asserts failed login with wrong password', ()=>{
-        loginPage.insertLoginDataAndClickSubmit(correctCredentials.email, incorrectPassword.password)
+    it('asserts failed login with wrong password', () => {
+        loginPage.insertLoginDataAndClickSubmit(loginData.correctCredentials.email, loginData.incorrectPassword.password)
         loginPageSelectors.alertBox().invoke('text').then(text => {
-            expect(text.trim()).to.eq(alertBoxMessages.baseAlert + alertBoxMessages.wrongPasswordAlert)
+            expect(text.trim()).to.eq(loginData.alertBoxMessages.baseAlert + loginData.alertBoxMessages.wrongPasswordAlert)
         })
     })
 
