4afa27637b9dbead408624ea05be1c6d8849d8b7
michal <michu214@gmail.com>
2024-08-08 18:58:02 +0200

    test change

--- cypress/e2e/loginFormTests.cy.js
+++ cypress/e2e/loginFormTests.cy.js
@@ -1,35 +1,37 @@
-import loginPage from "../support/pageObjects/adminStorePage/loginPage"
-import loginPageSelectors from "../support/selectors/loginPageSelectors"
-import loginData from "../fixtures/loginPage.json"
+// import loginPage from "../support/pageObjects/adminStorePage/loginPage"
+// import { incorrectMail, incorrectPassword, correctCredentials, alertBoxMessages }
+//     from "../fixtures/adminPage/loginPage.json"
+// import loginPageSelectors from "../support/selectors/loginPageSelectors"
 
-describe('login form tests', () => {
+describe('login success test', ()=>{
 
-    beforeEach(function () {
-        loginPage.visitLoginPage()
+    // beforeEach(() => {
+    //     loginPage.visitLoginPage()
+    // })
+    // afterEach(() => {
+    //     cy.clearLocalStorage()
+    // })
+
+    it('asserts login success', ()=>{
+        // loginPage.insertLoginDataAndClickSubmit(correctCredentials.email, correctCredentials.password)
+        // cy.url().should('eq', Cypress.config('baseUrl') + Cypress.config('panelMainPage'))
+        cy.visit('https://wp.pl')
+        cy.url().should('contain', 'wp')
     })
 
-    afterEach(() => {
-        cy.clearLocalStorage()
-    })
+    // it('asserts failed login with wrong mail', ()=>{
+    //     loginPage.insertLoginDataAndClickSubmit(incorrectMail.mail, correctCredentials.password)
+    //     loginPageSelectors.alertBox().invoke('text').then(text => {
+    //         expect(text.trim()).to.eq(alertBoxMessages.baseAlert + alertBoxMessages.wrongMailAlert)
+    //     })
+    // })
 
-    it('asserts login success', () => {
-        loginPage.insertLoginDataAndClickSubmit(loginData.correctCredentials.email, loginData.correctCredentials.password)
-        cy.url().should('eq', Cypress.config('baseUrl') + Cypress.config('panelMainPage'))
-    })
-
-    it('asserts failed login with wrong mail', () => {
-        loginPage.insertLoginDataAndClickSubmit(loginData.incorrectMail.mail, loginData.correctCredentials.password)
-        loginPageSelectors.alertBox().invoke('text').then(text => {
-            expect(text.trim()).to.eq(loginData.alertBoxMessages.baseAlert + loginData.alertBoxMessages.wrongMailAlert)
-        })
-    })
-
-    it('asserts failed login with wrong password', () => {
-        loginPage.insertLoginDataAndClickSubmit(loginData.correctCredentials.email, loginData.incorrectPassword.password)
-        loginPageSelectors.alertBox().invoke('text').then(text => {
-            expect(text.trim()).to.eq(loginData.alertBoxMessages.baseAlert + loginData.alertBoxMessages.wrongPasswordAlert)
-        })
-    })
+    // it('asserts failed login with wrong password', ()=>{
+    //     loginPage.insertLoginDataAndClickSubmit(correctCredentials.email, incorrectPassword.password)
+    //     loginPageSelectors.alertBox().invoke('text').then(text => {
+    //         expect(text.trim()).to.eq(alertBoxMessages.baseAlert + alertBoxMessages.wrongPasswordAlert)
+    //     })
+    // })
 
 })
 
