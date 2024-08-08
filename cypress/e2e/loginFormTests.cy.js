cc0a0516fc3090584066bb17bbc304f19da32cc9
michal <michu214@gmail.com>
2024-08-08 19:00:50 +0200

    revert changes

--- cypress/e2e/loginFormTests.cy.js
+++ cypress/e2e/loginFormTests.cy.js
@@ -1,37 +1,35 @@
-// import loginPage from "../support/pageObjects/adminStorePage/loginPage"
-// import { incorrectMail, incorrectPassword, correctCredentials, alertBoxMessages }
-//     from "../fixtures/adminPage/loginPage.json"
-// import loginPageSelectors from "../support/selectors/loginPageSelectors"
+import loginPage from "../support/pageObjects/adminStorePage/loginPage"
+import { incorrectMail, incorrectPassword, correctCredentials, alertBoxMessages }
+    from "../fixtures/adminPage/loginPage.json"
+import loginPageSelectors from "../support/selectors/loginPageSelectors"
 
 describe('login success test', ()=>{
 
-    // beforeEach(() => {
-    //     loginPage.visitLoginPage()
-    // })
-    // afterEach(() => {
-    //     cy.clearLocalStorage()
-    // })
-
-    it('asserts login success', ()=>{
-        // loginPage.insertLoginDataAndClickSubmit(correctCredentials.email, correctCredentials.password)
-        // cy.url().should('eq', Cypress.config('baseUrl') + Cypress.config('panelMainPage'))
-        cy.visit('https://wp.pl')
-        cy.url().should('contain', 'wp')
+    beforeEach(() => {
+        loginPage.visitLoginPage()
+    })
+    afterEach(() => {
+        cy.clearLocalStorage()
     })
 
-    // it('asserts failed login with wrong mail', ()=>{
-    //     loginPage.insertLoginDataAndClickSubmit(incorrectMail.mail, correctCredentials.password)
-    //     loginPageSelectors.alertBox().invoke('text').then(text => {
-    //         expect(text.trim()).to.eq(alertBoxMessages.baseAlert + alertBoxMessages.wrongMailAlert)
-    //     })
-    // })
+    it('asserts login success', ()=>{
+        loginPage.insertLoginDataAndClickSubmit(correctCredentials.email, correctCredentials.password)
+        cy.url().should('eq', Cypress.config('baseUrl') + Cypress.config('panelMainPage'))
+    })
 
-    // it('asserts failed login with wrong password', ()=>{
-    //     loginPage.insertLoginDataAndClickSubmit(correctCredentials.email, incorrectPassword.password)
-    //     loginPageSelectors.alertBox().invoke('text').then(text => {
-    //         expect(text.trim()).to.eq(alertBoxMessages.baseAlert + alertBoxMessages.wrongPasswordAlert)
-    //     })
-    // })
+    it('asserts failed login with wrong mail', ()=>{
+        loginPage.insertLoginDataAndClickSubmit(incorrectMail.mail, correctCredentials.password)
+        loginPageSelectors.alertBox().invoke('text').then(text => {
+            expect(text.trim()).to.eq(alertBoxMessages.baseAlert + alertBoxMessages.wrongMailAlert)
+        })
+    })
+
+    it('asserts failed login with wrong password', ()=>{
+        loginPage.insertLoginDataAndClickSubmit(correctCredentials.email, incorrectPassword.password)
+        loginPageSelectors.alertBox().invoke('text').then(text => {
+            expect(text.trim()).to.eq(alertBoxMessages.baseAlert + alertBoxMessages.wrongPasswordAlert)
+        })
+    })
 
 })
 
